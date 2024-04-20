33de07aec708f5c5d4de79d6e5a46315a0c682fb
TewodrosGebreslassie <abtedd5@gmail.com>
2024-04-19 22:58:22 -0500

    Project Start

---
+++ SD545/PROJECT/music-client/src/components/yourPlayList/YourPlayList.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+export default function YourPlayList() {
+  return (
+    <div>
+      <div>
+        <div className="yourPlayList">
+          <h2>Song Suggestions:</h2>
+          <table className="table table-hover">
+            <thead>
+              <tr>
+                <th scope="col">#</th>
+                <th scope="col">First</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">1</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">4</th>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+              </tr>
+              <tr>
+                <th scope="row">5</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+              <tr>
+                <th scope="row">6</th>
+                <td>Larry the Bird</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ SD545/PROJECT/music-client/src/pages/Login.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Login() {
+  const logIn = () => {};
+
+  return (
+    <div>
+      <button onClick={logIn}>Login</button>
+    </div>
+  );
+}
